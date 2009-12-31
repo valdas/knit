284f81f40d2b6dd729a5d3446c6f61c47c95c7c3
menzies.tim <menzies.tim@9b44de4e-a1c1-11de-bc7b-8ff00adae793>
2009-12-31 15:57:00 +0000

    mv outputs to $HOME/opt (less intrusive to people's directroy structure)

--- branches/0.2/lib/make/dirs.mk
+++ branches/0.2/lib/make/dirs.mk
@@ -1,7 +1,7 @@
-Expect ?= etc/expect#
+Tests  ?= etc/tests#
 Tmp    ?= $(HOME)/tmp#
-Html   ?= $(HOME)/var/html#
-Lib    ?= $(HOME)/var/lib#
+Html   ?= $(HOME)/opt/var/html#
+Lib    ?= $(HOME)/opt/var/lib#
 Old    ?= $(Lib)/old#
 
-Dirs ?= etc $(HOME)/var $(Expect) $(Tmp) $(Html) $(Lib) $(Old)
+Dirs ?= etc $(HOME)/var $(Tests) $(Tmp) $(Html) $(Lib) $(Old)
