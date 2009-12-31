4f1c2c0ca1f0e22c3a206f0157c138fa1d2020aa
menzies.tim <menzies.tim@9b44de4e-a1c1-11de-bc7b-8ff00adae793>
2009-12-31 15:57:49 +0000

    added "build" to the bulk rules (cahces, tests) so that before we run over everything, we rebuild everything

--- branches/0.2/lib/make/test.mk
+++ branches/0.2/lib/make/test.mk
@@ -2,20 +2,22 @@ run : #: run one eg (via make u=test run)
 	@$(MAKE) u=$u $u
 
 cache : #: run one eg and cache result (via make u=test cache)
-	@$(MAKE) run  | tee $(Expect)/$u
-	@echo new test result cached to $(Expect)/$u
+	@$(MAKE) run  | tee $(Tests)/$u
+	@echo new test result cached to $(Tests)/$u
 
-
-test : $(Expect)/$u #: test if the cached result is still current
+test : $(Tests)/$u #: test if the cached result is still current
 	@$(MAKE) run | tee $(Tmp)/$u.got 
-	@if  diff -s $(Tmp)/$u.got $(Expect)/$u > /dev/null;  \
+	@if  diff -s $(Tmp)/$u.got $(Tests)/$u > /dev/null;  \
 		then echo PASSED $u ; \
 		else echo FAILED $u,  got $(Tmp)/$u.got; \
 	fi
 
-tests : #: run 'test' on all egs
+caches :  build #: run 'cache' on all egs
+	@$(foreach x, $(Egs), $(MAKE) u=$x cache;) 
+
+tests : build #: run 'test' on all egs
 	@$(foreach x, $(Egs), $(MAKE) u=$x test;) 
 
 score : #: run 'test' on all egs, collect the scores
-	$(MAKE) tests | egrep '(PASSED|FAILED)' \
+	@$(MAKE) tests | egrep '(PASSED|FAILED)' \
          | gawk '{print $$1}' | uniq -c
