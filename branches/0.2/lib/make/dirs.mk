d2de480039a5f1750472aeb9f7c6fe7396c9da97
menzies.tim <menzies.tim@9b44de4e-a1c1-11de-bc7b-8ff00adae793>
2010-01-01 17:17:02 +0000

    Tags directory added

--- branches/0.2/lib/make/dirs.mk
+++ branches/0.2/lib/make/dirs.mk
@@ -3,5 +3,6 @@ Tmp    ?= $(HOME)/tmp#
 Html   ?= $(HOME)/opt/var/html#
 Lib    ?= $(HOME)/opt/var/lib#
 Old    ?= $(Lib)/old#
+Tags   ?= ../../../tags
 
-Dirs ?= etc $(HOME)/var $(Tests) $(Tmp) $(Html) $(Lib) $(Old)
+Dirs ?= etc $(HOME)/var $(Tests) $(Tmp) $(Html) $(Lib) $(Old) $(Tags)
