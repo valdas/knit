a6e7d00e5dcd67b44b33acd935107fc2cd9a4b96
menzies.tim <menzies.tim@9b44de4e-a1c1-11de-bc7b-8ff00adae793>
2010-01-02 02:09:20 +0000

    The tag builder stuff moved into "tricks" since it is quite generic

--- branches/0.2/lib/make/tricks.mk
+++ branches/0.2/lib/make/tricks.mk
@@ -6,6 +6,7 @@ include $(Knit)/lib/make/test.mk
 Egs  = $(shell grep '^eg' Makefile | sed 's/:.*//')
 I    = printf "\n----| $@ |--------------\n\n"
 
+
 help: about #: help
 	@printf "\nUsage  : make [OPTIONS]*\nOptions:\n"
 	@grep -h '^[a-zA-Z].*#:' $(Knit)/lib/make/*.mk Makefile  \
@@ -13,8 +14,23 @@ help: about #: help
 
 all : $(Egs) #: run all examples
 
+Download=$(Tags)/$(This)/$(Version)
+
+tag : build #: install the current version into tags
+	if [ ! -d "$(Download)" ]; then \
+		mkdir -p $(Download) ;   \
+		svn add $(This)      ;    \
+	fi
+	cd $(Download)
+	cp  $(LatestBuild) $(This)
+	svn add --quiet $(This)
+	cd ../..
+	svn commit -m "added : new version of $(This) $(Version)" 
+
+
 installDir : 
 	@$(foreach d, $(Dirs),\
 		 if [ ! -d $d ]; then echo mkdir -p $d; mkdir -p $d; fi; )
 
 install: installDir    # ensure everything is installed. warning: may be slow.
+
