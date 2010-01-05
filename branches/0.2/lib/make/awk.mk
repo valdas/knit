7e979383c9aac321260d56670330b3ddb2f20ef9
menzies.tim <menzies.tim@9b44de4e-a1c1-11de-bc7b-8ff00adae793>
2010-01-05 15:26:22 +0000

    isolated the gawk,pgawk references, made them all variables

--- branches/0.2/lib/make/awk.mk
+++ branches/0.2/lib/make/awk.mk
@@ -1,7 +1,7 @@
 # must be first
 include $(Knit)/lib/make/tricks.mk
 
-Uses    := $(shell gawk -f $(Knit)/lib/awk/uses.awk $(This).wak)#
+Uses    := $(shell $(Gawk) -f $(Knit)/lib/awk/uses.awk $(This).wak)#
 Awks    := $(subst .wak,.awk,$(Uses))
 LibAwks   :=  $(subst $(Lib)/ ,,$(Lib)/$(subst .awk ,.awk $(Lib)/,$(Awks)) $(Lib)/$(This).awk)#
 Loads   := -f $(subst .awk ,.awk -f ,$(LibAwks))
@@ -19,7 +19,7 @@ buildAwks: $(Tmp)/knit.tmp $(OldBuild) $(LatestBuild)
 buildHtmls: $(HtmlHtmls)
 
 $(Tmp)/knit.tmp : $(LibAwks)
-	@echo "#!`which gawk` -f " > $@
+	@echo "#!$(Gawk) -f " > $@
 	@(cat $(Knit)/etc/copyrite.txt; cat $(Knit)/etc/knit.txt) >> $@
 	@$(foreach f,$^,\
    	       (printf "\n#==== $f =============\n\n"; cat $f) >> $@;)
@@ -29,23 +29,23 @@ $(OldBuild)    : $(Tmp)/knit.tmp; @cp $< $@
 $(LatestBuild) : $(Tmp)/knit.tmp; @cp $< $@
 
 $(Lib)/%.awk : %.wak
-	@gawk -f $(Knit)/lib/awk/comment.awk $< > $@
+	@$(Gawk) -f $(Knit)/lib/awk/comment.awk $< > $@
 
 $(Html)/%.html : %.wak
-	gawk -f $(Knit)/lib/awk/markup.awk $< > $@
+	$(Gawk) -f $(Knit)/lib/awk/markup.awk $< > $@
 
 Vars = $(Tmp)/vars.out
 Profile = $(Tmp)/profile.out
-Spy  = pgawk --dump-variables="$(Vars)" \
+Spy  = $(Pgawk) --lint --dump-variables="$(Vars)" \
                     --profile="$(Profile)" $(Loads) #
 Dump = cat $(Profile) | sed '1d' ; cat $(Vars) | egrep -v '^[A-Z]+:'
-Run  = gawk  $(Loads)#
+Run  = $(Gawk)  $(Loads)#
 A    = $(Spy) -v Test=1  --source 'BEGIN {#
 Z    = ; exit}'
 a    = $(Run) -v Test=1 --source 'BEGIN {#
 z    = ; exit}'
 
-Demo=gawk 'BEGIN                     { FS="[ \t(]" } \
+Demo=$(Gawk) 'BEGIN                     { FS="[ \t(]" } \
            /^[ \t]function[ \t].*\(/ {print $$3"();"; exit}' $u.wak
 
 one :
