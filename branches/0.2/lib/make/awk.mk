8dd9e3b32ac59ad7a653db66a641241e1507931a
menzies.tim <menzies.tim@9b44de4e-a1c1-11de-bc7b-8ff00adae793>
2010-01-17 14:53:38 +0000

    tried some m4 pre-processing. unhappy with results. pulled it out
    added a "Gawk0" var so we can call a gawk direct in the make file

--- branches/0.2/lib/make/awk.mk
+++ branches/0.2/lib/make/awk.mk
@@ -1,7 +1,7 @@
 # must be first
 include $(Knit)/lib/make/tricks.mk
 
-Uses    := $(shell $(Gawk) -f $(Knit)/lib/awk/uses.awk $(This).wak)#
+Uses    := $(shell $(Gawk0) -f $(Knit)/lib/awk/uses.awk $(This).wak)
 Awks    := $(subst .wak,.awk,$(Uses))
 LibAwks   :=  $(subst $(Lib)/ ,,$(Lib)/$(subst .awk ,.awk $(Lib)/,$(Awks)) $(Lib)/$(This).awk)#
 Loads   := -f $(subst .awk ,.awk -f ,$(LibAwks))
@@ -18,21 +18,22 @@ buildAwks: $(Tmp)/knit.tmp $(OldBuild) $(LatestBuild)
 
 buildHtmls: $(HtmlHtmls)
 
-$(Tmp)/knit.tmp : $(LibAwks)	
+$(Tmp)/knit.tmp : $(LibAwks)	 
 	@echo $(LibAwks)
 	@echo "#!$(Gawk0) -f " > $@ 
 	@(cat $(Knit)/etc/copyrite.txt; cat $(Knit)/etc/knit.txt) >> $@
 	@echo "# Built on `date` by $(USER). " >> $@
 	@$(foreach f,$^,\
- 	       (printf "\n# $(shell basename $f) \n"; \
-               $(Gawk0) '/^#/{next}/^[ \t]*$$/{next}{print}'  $f) >> $@;)
+		(printf "\n# $(shell basename $f) \n"; \
+		$(Gawk0) '/^#/{next}/^[ \t]*$$/{next}{print}'  $f) >> $@;)
 	@chmod a+rx $@
 
 $(OldBuild)    : $(Tmp)/knit.tmp; @cp $< $@
 $(LatestBuild) : $(Tmp)/knit.tmp; @cp $< $@
 
 $(Lib)/%.awk : %.wak
-	 $(Gawk0) -f $(Knit)/lib/awk/comment.awk $< > $@
+	 cat $< | \
+         $(Gawk0) -f $(Knit)/lib/awk/comment.awk   > $@
 
 $(Html)/%.html : %.wak
 	$(Gawk0) -f $(Knit)/lib/awk/markup.awk $< > $@
@@ -49,12 +50,14 @@ a    = $(Run) -v Test=1 --source 'BEGIN {#
 z    = ; exit}'
 
 Demo=$(Pgawk) 'BEGIN                     { FS="[ \t(]" } \
-           /^[ \t]function[ \t]*demo.*\(/ {print $$3"();"; exit}' $u.wak
+           /^[ \t]function[ \t]*demo.*\(/ {print $$3"()"; exit}' $u.wak
 
 awk :
-	@echo "gawk $(Gawk)"
-	@echo "run $(Run)"
-	@echo "spy $(Spy)"
+	@printf "LibAwks $(LibAwks)\n\n"
+	@printf "gawk $(Gawk)\n\n"
+	@printf "run $(Run)\n\n"
+	@printf "spy $(Spy)\n\n"
+	@printf "$a $(One) $z"
 
-one :
+one : 
 	@$a $(One) $z
