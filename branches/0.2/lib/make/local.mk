38d682babe662871aead064497b19aeec935dd53
menzies.tim <menzies.tim@9b44de4e-a1c1-11de-bc7b-8ff00adae793>
2010-01-25 14:24:29 +0000

    changed location of the built files

--- branches/0.2/lib/make/local.mk
+++ branches/0.2/lib/make/local.mk
@@ -1,7 +1,10 @@
 ifeq (,$(findstring appele, $(MACHTYPE)))
-	Gawk0=/opt/local/bin/gawk
-	Gawk=$(Gawk0)
-	Pgawk=/opt/local/bin/gawk
+	Gawk0=/opt/local/bin/gawk#
+	Gawk=$(Gawk0)#
+	Pgawk=/opt/local/bin/gawk#
+	Lib=../site/src#
+        Html=../site#
+
 else	
         Gawk0=gawk
 	Gawk=gawk
