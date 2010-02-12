ba9c6e3225798a7b83966e91071fa0b4ab4a29a5
menzies.tim <menzies.tim@9b44de4e-a1c1-11de-bc7b-8ff00adae793>
2010-02-12 13:28:16 +0000

    stuff

--- branches/0.2/lib/make/dirs.mk
+++ branches/0.2/lib/make/dirs.mk
@@ -3,6 +3,6 @@ Tmp    ?= $(HOME)/tmp#
 Html   ?= $(HOME)/opt/var/html#
 Lib    ?= $(HOME)/opt/var/lib#
 Old    ?= $(Lib)/old#
-Tags   ?= ../../../tags
+Tags   ?= ../../../tags1
 
 Dirs ?= etc $(HOME)/var $(Tests) $(Tmp) $(Html) $(Lib) $(Old) $(Tags)
