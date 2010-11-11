f4625714e2ec93d36b949466f6850e915013850b
bbutler156 <bbutler156@9b44de4e-a1c1-11de-bc7b-8ff00adae793>
2010-11-11 18:47:35 +0000

    added htmls, code files

---
+++ branches/capstone/php/node.php
@@ -0,0 +1,19 @@
+<?php
+class node
+{
+//	var $left = new node;
+//	vae $right = new node;
+	private $number;
+
+
+	public function set($aValue)
+	{
+		$number=$aValue;
+	}
+
+	public function get()
+	{
+		return $number;
+	}
+}
+?>
