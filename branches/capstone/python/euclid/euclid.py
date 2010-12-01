9e72efdbb237e35433631ccbb2aea61090ad8543
Zach.Cowell <Zach.Cowell@9b44de4e-a1c1-11de-bc7b-8ff00adae793>
2010-12-01 19:56:00 +0000

    AUTO COMMIT: Project euclid Test test1 PASSED !

--- branches/capstone/python/euclid/euclid.py
+++ branches/capstone/python/euclid/euclid.py
@@ -13,12 +13,12 @@ def euclid(a,b):
 
 i = 0
 a = []
-f = open('/home/ccorder2/svns/knit/branches/capstone/python/euclid/tests/test1.in','r')
+f = open('tests/test1.in','r')
 for line in f:
         a.append(line)
 f.close()
 
-f = open('/home/ccorder2/svns/knit/branches/capstone/python/euclid/tests/tmp/test1.out','w')
+f = open('tests/tmp/test1.out','w')
 s = euclid(int(a[0]),int(a[1]))
 f.write(str(s))
 
