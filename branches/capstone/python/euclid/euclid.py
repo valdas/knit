9d6158af4d4fed65dccdb77e325bb0b0290ad4cd
ccorder2 <ccorder2@9b44de4e-a1c1-11de-bc7b-8ff00adae793>
2010-12-01 19:46:51 +0000

    updated euclid demo

--- branches/capstone/python/euclid/euclid.py
+++ branches/capstone/python/euclid/euclid.py
@@ -5,7 +5,20 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def euclid(a,b):
+
 	if b==0:
 		return a
 	else:
 		return euclid(b,(a%b))
+
+i = 0
+a = []
+f = open('/home/ccorder2/svns/knit/branches/capstone/python/euclid/tests/test1.in','r')
+for line in f:
+        a.append(line)
+f.close()
+
+f = open('/home/ccorder2/svns/knit/branches/capstone/python/euclid/tests/tmp/test1.out','w')
+s = euclid(int(a[0]),int(a[1]))
+f.write(str(s))
+
