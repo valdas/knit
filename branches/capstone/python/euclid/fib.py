90fb026da7fa5d04903bc9fd04eb50a6483f4a94
ccorder2 <ccorder2@9b44de4e-a1c1-11de-bc7b-8ff00adae793>
2010-12-08 16:09:24 +0000

    fib test

--- branches/capstone/python/euclid/fib.py
+++ branches/capstone/python/euclid/fib.py
@@ -11,3 +11,14 @@ def fib(n):
 		return 1
 	else:
 		return fib(n-1) + fib(n-2)
+
+i = 0
+a = []
+f = open('tests/tmp/infile','r')
+for line in f:
+        a.append(line)
+f.close()
+
+f = open('tests/tmp/outfile','w')
+s = fib(int(a[0]))
+f.write(str(s))
