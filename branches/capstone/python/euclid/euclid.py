c106ecb07e4755889a9569ccfad44a4d0d5a08ac
ccorder2 <ccorder2@9b44de4e-a1c1-11de-bc7b-8ff00adae793>
2010-12-08 11:49:29 +0000

    test

--- branches/capstone/python/euclid/euclid.py
+++ branches/capstone/python/euclid/euclid.py
@@ -13,12 +13,12 @@ def euclid(a,b):
 
 i = 0
 a = []
-f = open('/tests/tmp/infile','r')
+f = open('tests/tmp/infile','r')
 for line in f:
         a.append(line)
 f.close()
 
-f = open('/tests/tmp/outfile','w')
+f = open('tests/tmp/outfile','w')
 s = euclid(int(a[0]),int(a[1]))
 f.write(str(s))
 
